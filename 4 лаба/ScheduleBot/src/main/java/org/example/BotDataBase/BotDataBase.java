3eaccf42ce9b77465bc81ae3f2278d316d66085f
dsn777 <129421990+dsn777@users.noreply.github.com>
2023-12-14 15:31:32 +0300

    Add files via upload

---
+++ 4 лаба/ScheduleBot/src/main/java/org/example/BotDataBase/BotDataBase.java
@@ -0,0 +1,61 @@
+package org.example.BotDataBase;
+
+import java.sql.*;
+
+public class BotDataBase {
+    private final static String URL = "jdbc:mysql://localhost:3306/MyDataBase";
+    private final static String USER = "root";
+    private final static String PASSWORD = "user";
+
+    public static Connection connection;
+    public static Statement statement;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Подключение к базе данных успешно установлено!");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при подключении к базе данных:");
+            e.printStackTrace();
+        }
+    }
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Write(Long chatId, String userName,String firstName, String lastName) throws SQLException {
+
+        String query = "Select * from bot_users WHERE ChatId = " + chatId.toString();
+        ResultSet resultSet = statement.executeQuery(query);
+        if (resultSet.next())
+            return;
+
+        query = "INSERT INTO bot_users (" +
+                "ChatId, " +
+                "UserName," +
+                "FirstName, " +
+                "LastName) " +
+                "VALUES(" +
+                chatId + "," +
+                "'" + userName + "'" + "," +
+                "'" + firstName + "'" + "," +
+                "'" + lastName + "'" + ");";
+        statement.executeUpdate(query);
+    }
+}
+
+/*
+CREATE table bot_users
+(
+	ChatId INT,
+    UserName VARCHAR(40),
+    UserID INT,
+    FirstName varchar(40),
+    LastName varchar(40)
+)
+ */
+//SET SQL_SAFE_UPDATES = 0;
