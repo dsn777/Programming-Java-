af48c8871a00b5f493314e9c985fe3a2b46a044d
dsn777 <129421990+dsn777@users.noreply.github.com>
2024-03-15 23:17:32 +0300

    Add files via upload

---
+++ TestTask/src/main/java/org/example/ConnectionData.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ConnectionData {
+    final static String DATABASE = "MyDataBase";
+    final static String HOST = "jdbc:mysql://localhost:3306/";
+    final static String USER = "root";
+    final static String PASSWORD = "password";
+}
