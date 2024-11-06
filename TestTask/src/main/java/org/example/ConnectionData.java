159222ec875f4fdb95e5f7d64f0bd8224af3316c
dsn777 <129421990+dsn777@users.noreply.github.com>
2024-11-06 11:58:06 +0300

    Delete TestTask directory

--- TestTask/src/main/java/org/example/ConnectionData.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class ConnectionData {
-    final static String DATABASE = "MyDataBase";
-    final static String HOST = "jdbc:mysql://localhost:3306/";
-    final static String USER = "root";
-    final static String PASSWORD = "password";
-}
